76ef2af21538cca5a6356b1b4e80ca286996ff96
Jason <kangja1996@gmail.com>
2021-08-27 22:17:46 -0400

    progress

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="big-container">
+      <h1>To Do List</h1>
+      <form>
+        <label>
+          <input
+            className="input"
+            type="text"
+            placeholder="What are you working on today?"
+            list="list"
+          />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
