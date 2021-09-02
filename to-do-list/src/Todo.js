ee9cefd56675899141915c55c71d8da3e6faca9c
Jason <kangja1996@gmail.com>
2021-09-02 12:20:06 -0400

    fixed the issue of returning

--- to-do-list/src/Todo.js
+++ to-do-list/src/Todo.js
@@ -1,11 +1,11 @@
 import React from "react";
 
 function Todo({ todos }) {
-  return todos.map((todo, index) => {
+  return todos.map((todo, index) => (
     <div className="todo-row" key={index}>
-      <div>{todo.text}</div>;
-    </div>;
-  });
+      <div>{todo.text}</div>
+    </div>
+  ));
 }
 
 export default Todo;
